44987c42cd6109cc52e648415c5413ecc974f71e
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-10-27 05:35:44 +0300

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="description" content="Astro description" />
         <meta name="viewport" content="width=device-width" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
       </head>
       <body
         className={`overflow-y-auto overflow-x-hidden ${manrope.className} ${lora.variable}`}
