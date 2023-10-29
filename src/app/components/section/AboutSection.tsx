987984c1fb6de3cfef5b80728d085693fe70b2ac
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-10-29 09:40:24 +0300

    Update ui of about page

--- src/app/components/section/AboutSection.tsx
+++ src/app/components/section/AboutSection.tsx
@@ -1,15 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { MouseEventHandler, useRef } from "react";
+
+function calculatePercentage(x: number): number {
+  if (x > 40 && x < 60) {
+    return x;
+  } else if (x < 50) {
+    return x * 1.2;
+  } else {
+    return x * 0.9;
+  }
+}
 
 const AboutSection: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const circleSize = 130;
+
+  const handleMouseMove: MouseEventHandler<HTMLDivElement> = (ev) => {
+    if (!containerRef.current) return;
+    const rect = ev.currentTarget.getBoundingClientRect();
+    const { width, height } = containerRef.current?.getBoundingClientRect();
+
+    const x = Math.round(((ev.clientX - rect.left) / width) * 100);
+    const y = Math.round(((ev.clientY - rect.top) / height) * 100);
+
+    containerRef.current.style.setProperty(
+      "--x",
+      `min(${calculatePercentage(x)}%)`,
+    );
+    containerRef.current.style.setProperty(
+      "--y",
+      `${Math.max(Math.min(calculatePercentage(y), 85), 15)}%`,
+    );
+  };
+
   return (
-    <div className="container flex min-h-[100vh] flex-col justify-center">
-      <h2 className="pb-4 text-center text-4xl font-thin">About Me</h2>
-      <blockquote className="mx-auto w-5/6 py-8 sm:text-2xl md:text-3xl font-display">
-        I began my web development journey with a fascination for crafting
-        digital experiences and solving complex problems. I am a strong
-        proponent of writing safe, clean code because I believe it leads to more
-        robust, maintainable, and efficient applications.
-      </blockquote>
+    <div className="container flex min-h-[80vh] flex-col justify-center py-16">
+      <div
+        ref={containerRef}
+        className="relative flex-1 w-full py-8 text-4xl md:text-6xl font-display mx-auto"
+        onMouseMove={handleMouseMove}
+        onMouseLeave={() => {
+          containerRef.current?.style.setProperty("--size", "0px");
+        }}
+        onMouseEnter={() => {
+          containerRef.current?.style.setProperty("--size", `${circleSize}px`);
+        }}
+      >
+        <p className="px-6 md:px-12 absolute inset-0 flex flex-col justify-center z-10">
+          <b>Creating digital</b>
+          magic, pixel by pixel.
+        </p>
+        <p
+          style={{
+            clipPath: `circle(var(--size) at var(--x) var(--y))`,
+          }}
+          className="px-6 md:px-12 absolute inset-0 flex flex-col justify-center bg-primary text-white z-20 transition-all ease-linear"
+        >
+          <b>Turning visions</b>
+          into interactive web realities.
+        </p>
+      </div>
     </div>
   );
 };
