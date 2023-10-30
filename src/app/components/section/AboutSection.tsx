857e005857878ec01d08683e76dbab94a5f79dbb
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-10-30 18:45:35 +0300

    Refactor about section.

--- src/app/components/section/AboutSection.tsx
+++ src/app/components/section/AboutSection.tsx
@@ -26,26 +26,23 @@ const AboutSection: React.FC = () => {
     const x = Math.round(((ev.clientX - rect.left) / width) * 100);
     const y = Math.round(((ev.clientY - rect.top) / height) * 100);
 
-    containerRef.current.style.setProperty(
-      "--x",
-      `min(${calculatePercentage(x)}%)`,
-    );
+    containerRef.current.style.setProperty("--x", `${x}%`);
     containerRef.current.style.setProperty(
       "--y",
-      `${Math.max(Math.min(calculatePercentage(y), 85), 15)}%`,
+      `${Math.max(Math.min(y, 85), 20)}%`,
     );
+  };
+
+  const setSize = (size: string) => {
+    containerRef.current?.style.setProperty("--size", size);
   };
 
   return (
     <div className="container flex w-full justify-center py-40">
       <div
-        className="relative mx-auto h-[300px] w-full rounded-xl border-2 py-8 font-display text-4xl md:text-6xl"
-        onMouseEnter={() => {
-          containerRef.current?.style.setProperty("--size", `${circleSize}px`);
-        }}
-        onMouseLeave={() => {
-          containerRef.current?.style.setProperty("--size", "0px");
-        }}
+        className="relative mx-auto h-[300px] w-full rounded-xl border-2 py-8 font-display text-2xl sm:text-4xl md:text-6xl"
+        onMouseEnter={() => setSize(`${circleSize}px`)}
+        onMouseLeave={() => setSize("0px")}
         onMouseMove={handleMouseMove}
         ref={containerRef}
       >
@@ -57,10 +54,7 @@ const AboutSection: React.FC = () => {
           className="absolute inset-0 z-20 flex flex-col justify-center rounded-xl bg-primary px-6 text-white transition-all ease-linear md:px-12 "
           onClick={() => {
             containerRef.current?.style.removeProperty("transitionDuration");
-            containerRef.current?.style.setProperty(
-              "--size",
-              `${circleSize}px`,
-            );
+            setSize(`${circleSize}px`);
           }}
           style={{
             clipPath: `circle(var(--size, 0px) at var(--x, 50%) var(--y, 50%))`,
@@ -74,7 +68,7 @@ const AboutSection: React.FC = () => {
           className="absolute bottom-8 left-1/2 z-30 w-10 -translate-x-1/2 translate-y-1/2 transform cursor-pointer hover:invert"
           onClick={() => {
             if (!containerRef.current) return;
-            containerRef.current.style.setProperty("--size", "200%");
+            setSize("200%");
             containerRef.current.style.transitionDuration = "2s";
           }}
           src={NextArrow}
