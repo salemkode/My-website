6a580d70107d8ac0b684424a8907bb7a3a2ba6a0
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-11-01 08:39:06 +0300

    Remove Created with React message.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,8 @@
 import "server-only";
 
 let Footer = () => (
-  <footer className="bg-primary py-4 font-bold text-white">
-    <div className="container justify-between md:flex">
-      <h4 className="my-2"> All rights reserved {new Date().getFullYear()}</h4>
-      <h4 className="my-2">Created with React</h4>
-    </div>
+  <footer className="bg-primary py-4 text-center font-bold text-white">
+    <h4 className="my-2"> All rights reserved {new Date().getFullYear()}</h4>
   </footer>
 );
 
