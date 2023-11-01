db7cfe5a1c5b7e4991af240b1043ce261c87337c
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-11-01 12:26:18 +0300

    Add prefer const in eslint.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import "server-only";
 
-let Footer = () => (
+const Footer = () => (
   <footer className="bg-primary py-4 text-center font-bold text-white">
     <h4 className="my-2"> All rights reserved {new Date().getFullYear()}</h4>
   </footer>
