0e3e1352b18bcff57153d29b2818e84be057b7b6
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-11-02 06:24:45 +0300

    Remove h4 tag from footer.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="bg-primary py-4 text-center font-bold text-white">
-    <h4 className="my-2"> All rights reserved {new Date().getFullYear()}</h4>
+    All rights reserved {new Date().getFullYear()}
   </footer>
 );
 
